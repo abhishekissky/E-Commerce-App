3dc1b631b103957d5b2dde82163f73e6093d1328
abhis <abhishekissky@gmail.com>
2021-03-07 21:01:32 +0530

    Wish List and category

--- app/src/main/java/com/example/myapplication/fragments/WishListFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/WishListFragment.java
@@ -3,64 +3,58 @@ package com.example.myapplication.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplication.Adapter.WishListAdapter;
+import com.example.myapplication.Models.WishListModel;
 import com.example.myapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WishListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class WishListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerView;
+    List<WishListModel> wishListModels;
+    WishListAdapter wishListAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WishListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WishlistFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WishListFragment newInstance(String param1, String param2) {
-        WishListFragment fragment = new WishListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wishlist, container, false);
+        View view = inflater.inflate(R.layout.fragment_wishlist, container, false);
+        recyclerView = view.findViewById(R.id.wish_list_recycleView);
+        List<WishListModel>wishListModels1 = new ArrayList<>();
+        wishListModels1=getData();
+
+        WishListAdapter adapter = new WishListAdapter(getContext(),wishListModels1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+
+        return view;
+    }
+
+
+    private List<WishListModel> getData() {
+
+        wishListModels = new ArrayList<>();
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.kiwi));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.ponet));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.pears));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.ponet));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.ponet));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.ponet));
+        wishListModels.add(new WishListModel("Potato","20",R.drawable.ponet));
+
+        return wishListModels;
+
     }
 }
